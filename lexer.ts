9f5d132883a172388faade3f63a9df9e5b991a1c
owen <owenkirchenstien@gmail.com>
2025-01-02 14:07:21 -0500

    almost done w lex

---
+++ lexer.ts
@@ -0,0 +1,38 @@
+export enum TokenType {
+    Number,
+    Identifier,
+    Equals,
+    OpenParen,
+    CloseParen,
+    BinaryOperator,
+    Let,
+}
+
+export interface Token {
+    value: string;
+    type: TokenType;
+}
+
+function token(value = "", type: TokenType): Token {
+    return { value, type };
+}
+
+
+export function tokenize (sourceCode: string): Token[] {
+    const tokens = new Array<Token>();
+    const src = sourceCode.split("");
+
+    while (src.length > 0) {
+        if (src[0] == ')') {
+            tokens.push(token(src.shift(), TokenType.OpenParen));
+        } else if (src[0] == ")") {
+            tokens.push(token(src.shift(), TokenType.CloseParen));
+        } else if (src[0] == "+" || src[0] == "-") {
+            tokens.push(token(src.shift(), TokenType.BinaryOperator));
+        } else if (src[0] == "*" || src[0] == "/") {
+            tokens.push(token(src.shift(), TokenType.BinaryOperator));
+    }
+}
+
+    return tokens;
+}
